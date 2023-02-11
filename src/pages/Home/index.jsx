93b7a728a490f2b53a8a8d6dfdd43ed62a59cc78
crystian97 <icrystianveloso@gmail.com>
2023-02-11 17:55:21 -0300

    feat:Homepage initial version

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,10 @@
+import Header from "../../components/Header";
+import { HomeContainer } from "./styles";
+
+export default function Home() {
+  return (
+    <HomeContainer>
+      <Header />
+    </HomeContainer>
+  );
+}
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const HomeContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 0px 18px;
+`;
