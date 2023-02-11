5640339ef47fe7f3a6d48a67df45ef88a214b01f
crystian97 <icrystianveloso@gmail.com>
2023-02-11 15:36:24 -0300

    feat:create initial globalStyle to project

---
+++ src/styles/globalStyles.js
@@ -0,0 +1,10 @@
+const { createGlobalStyle } = require("styled-components");
+
+export const GlobalStyles = createGlobalStyle`
+
+*{
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+}
+`;
