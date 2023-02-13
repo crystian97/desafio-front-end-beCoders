2b5a46eaf9531b44e781ee570beada572a5eb34f
crystian97 <icrystianveloso@gmail.com>
2023-02-12 22:38:53 -0300

    feat:getting home videos v1

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,28 @@
 import Header from "../../components/Header";
 import { HomeContainer } from "./styles";
+import { API } from "../../services/Api";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [homeVideos, setHomeVideos] = useState([]);
+
+  async function getHomeVideos() {
+    const videos = await API.get("/videos", {
+      params: {
+        chart: "mostPopular",
+        key: "AIzaSyAQ1rdGyNlDJtVC-2hTeQJPoR4Jh9bSQHs",
+      },
+    })
+      .then((response) => {
+        response = response.data.items;
+        setHomeVideos(response);
+      })
+      .catch((e) => console.log(e));
+  }
+  console.log(homeVideos);
+  useEffect(() => {
+    getHomeVideos();
+  }, []);
   return (
     <HomeContainer>
       <Header />
